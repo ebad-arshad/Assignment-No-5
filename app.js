711ea191c97c146ab130164732f5c5345dab017c
M.Ebad Arshad <104248177+ebad-arshad@users.noreply.github.com>
2022-06-01 15:55:43 +0500

    completed assignment 5

---
+++ app.js
@@ -0,0 +1,118 @@
+// Chp 17-20
+
+// Q1
+
+// var multiArray = [[],[]];
+
+// Q2
+
+// var multiArray = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+
+// Q3
+
+// for (var i = 1; i <= 10; i++) {
+//     document.write(i + "<br />")
+// }
+
+// Q4
+
+// var num = +prompt("Enter Number");
+// var length = +prompt("Enter Length");
+// for (var i = 1; i <= length; i++) {
+//     document.write(num + " x " + i + " = " + num*i + "<br />")
+// }
+
+// Q5
+
+// var fruits = ["apple","banana","mango","orange","strawberry"]
+// for(var i= 0; i < fruits.length; i++) {
+//     document.write(fruits[i] + "<br />");
+// }
+// document.write("<br />");
+// for(var i= 0; i < fruits.length; i++) {
+//     document.write("Element at index " + i + " is " + fruits[i] + "<br />");
+// }
+
+// Q6
+
+// document.write("<h1> Counting: </h1>");
+// for (var i = 1; i <= 15; i++) {
+//     document.write(i + ",")
+// }
+// document.write("<br />");
+// document.write("<h1> Reverse counting: </h1>");
+// for (var j = 10; j >= 1; j--) {
+//     document.write(j + ",")
+// }
+// document.write("<br />");
+// document.write("<h1> Even: </h1>");
+// for (var k = 0; k <= 20; k = k + 2) {
+//     document.write(k + ",")
+// }
+// document.write("<br />");
+// document.write("<h1> Odd: </h1>");
+// for (var l = 1; l <= 19; l = l + 2) {
+//     document.write(l + ",")
+// }
+// document.write("<br />");
+// document.write("<h1> Series: </h1>");
+// for (var m = 2; m <= 20; m = m + 2) {
+//     document.write(m + "k" + ",")
+// }
+
+// Q7
+
+// var user = prompt();
+// var a = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var flag = false;
+// for (var i = 0; i < a.length; i++) {
+
+//     if (user === a[i]) {
+//         flag = true;
+//         document.write(user + " is available at index " + i + " in our bakery");
+//         break;
+//     }
+
+// }
+// if (flag === false) {
+//     document.write("We are sorry. " + user + " is not available in our bakery")
+// }
+
+// Q8
+
+// var a = [24, 53, 78, 91, 12];
+// var sort = a.sort().reverse();
+// var copy = a.slice(0,1);
+// console.log(copy)
+
+// Q9
+
+// var a = [24, 53, 78, 91, 12];
+// var sort = a.sort();
+// var copy = a.slice(0,1);
+// console.log(copy)
+
+// Q10
+
+// for (var i = 1; i <= 20; i++) {
+//     document.write(5*i + ",");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
